62ab52613c7217f2bc85b198bc48ae69792147fc
DEHBIA-HANI <129846731+DEHBIA-HANI@users.noreply.github.com>
2024-09-25 20:26:33 +0200

    le style de nav et home et  création du fichier json

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import logo from "../assets/images/logo.png";
-import Content from "./Content";
+import logo from "../assets/images/logo.webp";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
@@ -44,7 +43,6 @@ const Header = () => {
             </li>
           </ul>
         </nav>
-        <Content />
       </header>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-
+import portrait from "../assets/images/Dehbia1.webp";
 const Home = () => {
   return (
-    <div className="home-page">
-      <h1>Aprops</h1>
-    </div>
+    <section className="header_content">
+      <div className="part_left">
+        <h1>Bonjour ,</h1>
+        <h2>Je suis Dehbia Hachi</h2>
+
+        <p>
+          Développeuse front-end diplômée, je me suis reconvertie après 6 ans
+          comme infirmière. Passionnée par le web, j’allie rigueur et créativité
+          pour concevoir des interfaces performantes et esthétiques, avec une
+          attention particulière à l’expérience utilisateur.
+        </p>
+      </div>
+      <div className="part_right">
+        <p>
+          <img src={portrait} alt="portrait Dehbia Hachi" />
+        </p>
+      </div>
+    </section>
   );
 };
 
