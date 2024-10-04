c979545086f8e8bfcf585bf23c0c407ff279f923
DEHBIA-HANI <129846731+DEHBIA-HANI@users.noreply.github.com>
2024-10-04 10:33:08 +0200

    mettre tous de open grraphe sur index HTML

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,13 @@ import React from "react";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
-import { Helmet } from "react-helmet";
+// import { Helmet } from "react-helmet";
 
 const App = () => {
   return (
     <>
-      <Helmet>
-        <meta
-          name="application-name"
-          content="Dehbia Hachi - Développeur Front-End"
-        />
-        <meta name="theme-color" content="#1d3b31" />
-        <meta name="msapplication-TileColor" content="#1d3b31" />
-        <meta
-          name="description"
-          content="Développeuse Front-End experte en JavaScript et React. Découvrez mon portfolio et mes compétences en création d'interfaces web dynamiques et performantes."
-        />
-        <meta property="og:type" content="website" />
-        {/* Open Graph Meta Tags */}
-        <meta
-          property="og:title"
-          content="Dehbia Hachi - Développeur Front-End"
-        />
-        <meta
-          property="og:description"
-          content="Développeuse Front-End experte en JavaScript et React."
-        />
-        <meta property="og:url" content="https://dehbia.vercel.app/" />{" "}
-        {/* Twitter Meta Tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:title"
-          content="Dehbia Hachi - Développeur Front-End"
-        />
-        <meta
-          name="twitter:description"
-          content="Développeuse Front-End experte en JavaScript et React."
-        />
-        <meta name="twitter:url" content="https://dehbia.vercel.app/" />{" "}
-      </Helmet>
+      {/* <Helmet>
+      </Helmet> */}
       <BrowserRouter>
         <Header />
         <Home />
